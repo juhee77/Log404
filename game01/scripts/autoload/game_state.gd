4bad1c3513af4d1d22de14548ba5b7df7574b19b
juhee77 <bora2233@naver.com>
2026-08-06 07:10:35 +0900

    Module 37: Add recliner sofa study pod system

--- game01/scripts/autoload/game_state.gd
+++ game01/scripts/autoload/game_state.gd
@@ -158,6 +158,14 @@ var upgrades: Dictionary = {
 		"cost_mult": 1.8,
 		"desc": "소음을 줄이고 몰입감을 최대로 높여주는 아날로그 백색소음"
 	},
+	"recliner_pod": {
+		"name": "🛋 1인 전용 무중력 리클라이너 소파 포드",
+		"level": 0,
+		"max_level": 3,
+		"base_cost": 2400.0,
+		"cost_mult": 2.2,
+		"desc": "180도 각도 조절 1인 프리미엄 몰입 포드로 체류 시간 +30% 증가"
+	},
 	"cloud_printer": {
 		"name": "🖨 모바일 무인 스마트 프린터 & 스캐너",
 		"level": 0,
