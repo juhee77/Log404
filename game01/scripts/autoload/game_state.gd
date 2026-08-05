06f1e1aaa815cdf7ec80b32f05563eb3271d25e4
juhee77 <bora2233@naver.com>
2026-08-06 06:53:10 +0900

    Module 22: Add lounge relaxation massage chair and stretching zone upgrade

--- game01/scripts/autoload/game_state.gd
+++ game01/scripts/autoload/game_state.gd
@@ -158,6 +158,14 @@ var upgrades: Dictionary = {
 		"cost_mult": 1.8,
 		"desc": "소음을 줄이고 몰입감을 최대로 높여주는 아날로그 백색소음"
 	},
+	"massage_chair": {
+		"name": "🧘 휴게실 고급 안마의자 & 스트레칭존",
+		"level": 0,
+		"max_level": 3,
+		"base_cost": 1500.0,
+		"cost_mult": 2.0,
+		"desc": "공부 피로도를 전신 마사지로 풀어주어 손님 만족도 및 체류 시간 +25% 증가"
+	},
 	"branch_expansion": {
 		"name": "🏢 2호점 강남 직영점 오픈",
 		"level": 0,
